ebc415a217d64b8dee8a2a0bda415e8db4cce27f
LLQT <thinhle.hardware@gmail.com>
2024-09-29 17:47:56 +0700

    commit after finsh TRANG IU <3

--- STM32_cube_IDE/Core/Src/Ex9.c
+++ STM32_cube_IDE/Core/Src/Ex9.c
@@ -2,7 +2,15 @@
 const int MAX_LED_MATRIX = 8;
 int index_led_matrix = 0;
 //uint8_t matrix_buffer[8] = {0x3C, 0x42, 0x42, 0x7E, 0x42, 0x42, 0x42, 0x42};
-uint8_t matrix_buffer[8] = {0x00, 0xFE, 0x09, 0x09, 0x09, 0x09, 0xFE, 0x00};
+uint8_t matrix_buffer[8] = {0x01, 0x01, 0x01, 0xFF, 0xFF, 0x01, 0x01, 0x01};//use to indicate led matrix
+uint8_t char_T [8] = {0x01, 0x01, 0x01, 0xFF, 0xFF, 0x01, 0x01, 0x01};
+uint8_t char_I [8] = {0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00};
+uint8_t char_R [8] = {0xFF, 0x05, 0x0D, 0x15, 0x25, 0x45, 0x85, 0x07};
+uint8_t char_N [8] = {0xFF, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0xFF};
+uint8_t char_G [8] = {0x7E, 0x91, 0x91, 0x91, 0x91, 0x91, 0x91, 0x62};
+uint8_t char_U [8] = {0x00, 0x7F, 0x80, 0x80, 0x80, 0x80, 0x7F, 0x00};
+uint8_t char_A [8] = {0x00, 0xFE, 0x09, 0x09, 0x09, 0x09, 0xFE, 0x00};
+uint8_t char_heart [8] = {0x1E, 0x21, 0x41, 0x82, 0x82, 0x81, 0x41, 0x1E};
 uint16_t matrix_Col[8]= {0x0010, 0x0020, 0x0040, 0x0080, 0x0100, 0x0200, 0x0400, 0x0800};
 uint8_t reset_row = 0x00FF;
 uint16_t reset_col = 0x0FF0;
@@ -34,14 +42,40 @@ void Ex9_time()
 	if(index_led_matrix > 7)
 	{
 		index_led_matrix = 0;
-		uint8_t temp = matrix_buffer[0];
+		uint8_t temp [8];
+		temp [0] = matrix_buffer[0]; //char_T
+		temp[1] = char_R[0];
+		temp[2] = char_A[0];
+		temp[3] = char_N[0];
+		temp[4] = char_G[0];
+		temp[5] = char_I[0];
+		temp[6] = char_U[0];
+		temp[7] = char_heart[0];
+
+		//init value of matrix_buffer is char_T
 
 		for(int i = 0 ; i < 7 ; i++)
 		{
-			matrix_buffer[i] = matrix_buffer[i+1];
+			matrix_buffer[i] = matrix_buffer[i+1];//T
+			char_R[i] = char_R[i+1];
+			char_A[i] = char_A[i+1];
+			char_N[i] = char_N[i+1];
+			char_G[i] = char_G[i+1];
+			char_I[i] = char_I[i+1];
+			char_U[i] = char_U[i+1];
+			char_heart[i] = char_heart[i+1];
 		}
 
-		matrix_buffer [7] = temp;
+		matrix_buffer[7] = temp[1];
+		char_R[7] = temp[2];
+		char_A[7] = temp[3];
+		char_N[7] = temp [4];
+		char_G[7] = temp[5];
+
+		char_I[7] = temp[6];
+		char_U[7] = temp[7];
+
+		char_heart[7] = temp [0];
 	}
 
 	updateLEDMatrix(index_led_matrix);
